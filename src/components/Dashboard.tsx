bc5413a0201910743d80ffc932da69fd66a20835
chods <johnkennethpteodoro@gmail.com>
2025-01-11 12:25:26 +0800

    Enhance UI

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,8 @@ import Header from './Header';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import MyChart from './MyChart';
-
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Star } from 'lucide-react';
 function Dashboard() {
   return (
     <>
@@ -75,6 +76,41 @@ function Dashboard() {
           </Card>
         </div>
         <MyChart />
+        <Card className="mt-10">
+          <CardHeader>
+            <CardTitle className="text-zinc-500">Recent Reviews</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="mb-4 flex items-center gap-2">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <h1 className="text-[20px]">Kenneth</h1>
+            </div>
+            <div className="bg-red mb-2 flex gap-2">
+              <Star className="text-yellow-400" />
+              <Star className="text-yellow-400" />
+              <Star className="text-yellow-400" />
+              <Star className="text-yellow-400" />
+              <Star className="text-yellow-400" />
+            </div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+              consequuntur saepe voluptatem reprehenderit nostrum ratione.
+              Beatae sunt repudiandae quod ipsum, doloremque ipsam corporis
+              facilis illum minima eius cupiditate eligendi labore!
+            </p>
+            <div className="flex justify-end">
+              <div>
+                <Badge variant="outline" className="mb-2 rounded">
+                  19.00%
+                </Badge>
+                <p className="text-[12px]">vs prev preiod</p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Header() {
   return (
     <>
-      <div className="w-full border-b border-black px-5 py-5">
-        <h1 className="text-[20px] font-extrabold">Crowd Resourcing</h1>
+      <div className="w-full border-b border-black px-5 py-7">
+        <h1 className="text-[24px] font-extrabold">Crowd Sourcing</h1>
       </div>
     </>
   );
